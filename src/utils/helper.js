e7212a771a1741c87171f1c60c207414f8222ffe
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-02-12 23:06:03 +0800

    Update helper.js

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -68,16 +68,16 @@ export const getCurrentDateTime = () => {
 const monthMapping = {
     'Januari': 1,
     'Februari': 2,
-    'Maret': 3,
+    'Mac': 3,
     'April': 4,
     'Mei': 5,
-    'Juni': 6,
-    'Juli': 7,
-    'Agustus': 8,
+    'Jun': 6,
+    'Julai': 7,
+    'Ogos': 8,
     'September': 9,
     'Oktober': 10,
     'November': 11,
-    'Desember': 12
+    'Disember': 12
 };
 
 export const monthNameToNumber = (monthName) => {
