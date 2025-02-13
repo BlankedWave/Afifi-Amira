4a2e1f1fc3cd318827e231b7e92e91423a99cfbb
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-02-13 16:05:49 +0800

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -41,7 +41,7 @@ export const data = {
         //         finish: 'Selesai'
         //     }
         // },
-        address: 'Kp. Lorem, RT 000/ RW 000, Desa.Lorem, Kec.Ipsum, Kab.Lorem, Lorem (1234)'
+        address: 'Alamat Tempat Majlis'
     },
 
     link: {
